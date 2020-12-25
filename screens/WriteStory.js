08c55cf099b00d3281a37d94f39443649232b22c
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2020-12-25 16:25:15 +0530

    Add files via upload

---
+++ screens/WriteStory.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TextInput, TouchableOpacity} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+
+export default class WriteStory extends React.Component {
+ constructor () {
+   super();
+   this.state = {
+     title : "",
+     author : "",
+     storytext : ""
+   } 
+ }
+  submitStory = () => {
+     db.collection("stories").add({
+       "title" : title,
+       "author" : author,
+       "storytext" : storytext
+     }) 
+     this.setState({
+      title : "",
+      author : "",
+      storytext : "" 
+    })
+     return alert("Your Story Is Added :)")
+  }
+    render() {
+      return (
+        <View style={{flex: 1}}>
+
+          <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Title of your story"}
+          onChangeText={()=>{
+            this.setState({
+              title : text
+            })
+          }}
+        /> 
+
+   <TextInput
+          style={styles.formTextInput}
+          placeholder ={"Author of the story"} 
+          onChangeText={()=>{
+            this.setState({
+              author : text
+            })
+          }}
+        /> 
+
+<TextInput
+          style={styles.writeStory}
+          placeholder ={"Write your story"} 
+          onChangeText={()=>{
+            this.setState({
+              storytext : text
+            })
+          }}
+        /> 
+
+        <TouchableOpacity style={styles.submitButton}
+        onPress={()=>{this.submitStory()}}
+        >
+          <Text style={styles.buttonText}> Submit </Text>
+        </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    formTextInput : {
+      marginTop: 100,
+      width: '80%',
+      alignSelf: 'center',
+      height: 40,
+      textAlign: 'center',
+      borderWidth: 2,
+      outline: 'none',
+    } ,
+ writeStory : {
+        marginTop: 100,
+        width: '80%',
+        alignSelf: 'center',
+        height: 100,
+        textAlign: 'center',
+        borderWidth: 2,
+        outline: 'none',
+      } ,
+ submitButton:{
+      backgroundColor:'#EDC0BF',
+      width: 50,
+      borderWidth:1.5,
+     },
+     buttonText : {
+       fontSize : 30,
+       fontWeight : "bold",
+       color : "black"
+     }
+  })
